557d13c60a29e158828ff0066cc7b2f4dc6e6b26
Tiffany Choong <tiffany.choong@rbc.com>
2020-11-29 22:04:40 -0500

    ⚡️ Add input component; add vue config for sass vars

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
-	<Kirby />
+	<div class="container">
+		<dreamy-input 
+			v-model="emailValue" 
+			@keydown.enter="submitMail"
+		/>
+		<kirby />
+	</div>
 </template>
 
 <script>
+import DreamyInput from './components/DreamyInput.vue'
 import Kirby from './components/Kirby.vue'
 
 export default {
 	name: 'App',
 	components: {
-		Kirby
+		Kirby,
+		DreamyInput
+	},
+	data() {
+		return {
+			emailValue: ''
+		}
+	},
+	methods: {
+		submitMail() {
+			console.log(this.emailValue);
+		}
 	}
 }
 </script>
---
+++ src/components/DreamyInput.vue
@@ -0,0 +1,64 @@
+<template>
+	<div class="dreamy-input">
+		<input 
+			:value="modelValue"
+			class="dreamy-input-control"
+			type="text"
+			@input="$emit('update:modelValue', $event.target.value)"
+		>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'dreamy-input',
+	props: {
+		modelValue: String
+	}
+}
+</script>
+
+<style lang="scss">
+.dreamy-input {
+	position: relative;
+
+	&::after {
+		content: '';
+		position: absolute;
+		top: 3px;
+		left: -2px;
+		height: 100%;
+		width: 100%;
+		border: 3px solid var(--black);
+		pointer-events: none;
+		transition: border 0.4s ease-in-out;
+	}
+
+	&::after,
+	&-control {
+		border-radius: 3.125rem;
+	}
+
+	&-control {
+		width: 100%;
+		height: 3.125rem;
+		padding: 0 1rem;
+		border: 0;
+		background-color: var(--white);
+		font-family: $primary-font;
+		font-size: 1.25rem;
+		color: var(--black);
+		caret-color: var(--secondary-color);
+
+		&:focus {
+			outline: none;
+		}
+	}
+
+	&:focus-within {
+		&::after {
+			border-color: var(--secondary-color);
+		}
+	}
+}
+</style>
